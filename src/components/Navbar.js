f3d850235cda8e22c8a34aff9e9850c3069212a5
Itzel <104706728+Itzelmariana@users.noreply.github.com>
2022-12-26 20:50:15 -0600

    edit navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import SearchBar from './SearchBar';
 
 import './Navbar.css';
 
 export default function Navbar() {
   return (
     <nav className='navbar'>
-      <h1> Recipe Directory </h1>
-      <NavLink to='/'>Home</NavLink>
-      <NavLink to='/create'>Create</NavLink>
-      <NavLink to='/search'>Search</NavLink>
-      <NavLink to='/recipes/:id'>Recipes</NavLink>
+      <h1>
+        <NavLink to='/recipes/:id'>Recipe Directory </NavLink>
+      </h1>
+
+      <SearchBar />
+
+      <NavLink to='/create' className='box'>
+        Create Recipe
+      </NavLink>
     </nav>
   );
 }
